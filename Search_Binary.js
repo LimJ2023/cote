bf7aa3a0479c851e36470dd0554542a916f87165
Lim J <heuristic2022@gmail.com>
2024-10-12 12:27:59 +0900

    이진탐색 구현

--- Search_Binary.js
+++ Search_Binary.js
@@ -1,6 +1,7 @@
 //이진검색은 정렬된 배열을 받는다.
+
+//내가 만들어본 코드
 function binarySearch(arr, val) {
-  // add whatever parameters you deem necessary - good luck!
   let left = 0;
   let right = arr.length - 1;
   let middle = Math.floor(right / 2);
@@ -17,4 +18,21 @@ function binarySearch(arr, val) {
     }
   }
   return -1;
+}
+
+//정리된 코드
+function binarySearch2(arr, elem) {
+  let start = 0;
+  let end = arr.length - 1;
+  let middle = Math.floor((start + end) / 2);
+
+  while (arr[middle] != elem && start <= end) {
+    if (elem < arr[middle]) {
+      end = middle - 1;
+    } else {
+      start = middle + 1;
+    }
+    middle = Math.floor((start + end) / 2);
+  }
+  return arr[middle] === elem ? middle : -1;
 }

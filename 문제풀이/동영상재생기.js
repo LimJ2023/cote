77b7e38b0542e658a46506443bab49cabb681a29
Lim J <heuristic2022@gmail.com>
2024-11-29 12:18:44 +0900

    그래프 DFS

---
+++ 문제풀이/동영상재생기.js
@@ -0,0 +1,18 @@
+function solution() {
+  console.log("34:33".split(":"));
+
+  function calcTime(time, command) {
+    const timeArr = time.split(":");
+    command;
+    let minuete = timeArr[0];
+    let second = timeArr[1];
+
+    let resultSec = (second + input) % 60;
+    let addMin = Math.floor((minuete + input) / 60);
+
+    minuete += addMin;
+  }
+  var answer = "";
+  return answer;
+}
+solution();
